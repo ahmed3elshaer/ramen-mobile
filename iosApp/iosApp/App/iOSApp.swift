208fb88c6504dc44fb1a9605f7eedf8cb58f106d
MageBuild <ahmed3elshaer@gmail.com>
2023-12-23 06:19:04 +0200

    feat: Add support for donations
    This commit adds the ability for users to donate to support our project. The donations feature includes a view with information about the project and a button to make a donation. The view includes a gradient background that animates and changes colors. The functionality includes handling payment transactions using StoreKit and a PaymentObserver class.

--- iosApp/iosApp/App/iOSApp.swift
+++ iosApp/iosApp/App/iOSApp.swift
@@ -2,29 +2,34 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 	var body: some Scene {
 		WindowGroup {
-            TabView {
-                MonitorScreen()
-                           .tabItem {
-                               Image(systemName: "refrigerator.fill")
-                               Text("Fridge")
-                       }
-                
-                StoreIngredientsScreen()
-                           .tabItem {
-                               Image(systemName: "plus.circle")
-                               Text("Store")
-                       }
-                       RecipeScreen()
-                           .tabItem {
-                               Image(systemName: "fork.knife.circle")
-                               Text("Recipes")
-                       }
-            }
-            .background(VisualEffect(style: .systemThinMaterial))
-            .accentColor(Color.defaultPrimary)
+			TabView {
+				MonitorScreen()
+						.tabItem {
+							Image(systemName: "refrigerator.fill")
+							Text("Fridge")
+						}
+
+				StoreIngredientsScreen()
+						.tabItem {
+							Image(systemName: "plus.circle")
+							Text("Store")
+						}
+				RecipeScreen()
+						.tabItem {
+							Image(systemName: "fork.knife.circle")
+							Text("Recipes")
+						}
+                DonationsView()
+						.tabItem {
+							Image(systemName: "heart.fill")
+							Text("Support Us")
+						}
+			}
+					.background(VisualEffect(style: .systemThinMaterial))
+					.accentColor(Color.defaultPrimary)
 		}
 	}
 }
